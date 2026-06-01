72e0e465ad81f216b98c262ced9c9c2d9df240ee
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2026-05-31 22:51:21 -0700

    VkShaderModule instantiation boilerplate
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -7,6 +7,7 @@
 //#include <vulkan/vulkan.h>
 
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <optional>
@@ -202,6 +203,24 @@ private:
         }
 
         return extensions;
+    }
+
+    static std::vector<char> readFile(const std::string& filename) {
+        std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+        if (!file.is_open()) {
+            throw std::runtime_error("failed to open file!");
+        }
+
+        const size_t fileSize = (size_t) file.tellg();
+        std::vector<char> buffer(fileSize);
+
+        file.seekg(0);
+        file.read(buffer.data(), fileSize);
+
+        file.close();
+
+        return buffer;
     }
 
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
@@ -575,8 +594,30 @@ private:
         }
     }
 
+    VkShaderModule createShaderModule(const std::vector<char> &code) {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        /* Vulkan mandates SPIR-V bytecode is stored in little-endian format */
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+        VkShaderModule shaderModule;
+        if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create shader module!");
+        }
+
+        return shaderModule;
+    }
+
     void createGraphicsPipeline() {
-        
+        auto vertShaderCode = readFile("shaders/vert.spv");
+        auto fragShaderCode = readFile("shaders/frag.spv");
+
+        VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+        VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+        vkDestroyShaderModule(device, fragShaderModule, nullptr);
+        vkDestroyShaderModule(device, vertShaderModule, nullptr);
     }
 
     void initVulkan() {
